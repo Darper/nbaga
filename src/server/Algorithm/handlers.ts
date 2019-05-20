1ea064ba6253796b903fd7821ba68a4263248ce3
Derek Harper <dharper@digitalriver.com>
2019-05-20 10:11:45 -0500

    Algorithm is running and sorting, but unable to print out formulas and I think each player is getting a different equation. Lots of work left to do.

--- src/server/Algorithm/handlers.ts
+++ src/server/Algorithm/handlers.ts
@@ -5,21 +5,27 @@ import { operators, columns, populationSize } from "../common/variables";
 
 const router: Router = Router();
 
-interface IPopulation [
-    
-]
+interface IResult {
+  value: number;
+  child: string;
+}
 
 function getRandomArbitrary(min: number, max: number) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function newTree(player: { [index: string]: IPlayer }): string {
   const numberOrOperator = Math.floor(Math.random() * 100);
   if (numberOrOperator < 52) {
-    const randomAttributeIndex = getRandomArbitrary(0, columns.length);
-    const randomAttribute = columns[randomAttributeIndex];
-    const playerAttributeValue = player[randomAttribute];
-    return playerAttributeValue.toString();
+    if (numberOrOperator < 26) {
+      const randomAttributeIndex = getRandomArbitrary(0, columns.length);
+      const randomAttribute = columns[randomAttributeIndex];
+      const playerAttributeValue = player[randomAttribute];
+      return playerAttributeValue.toString();
+    } else {
+      const randomValue = Math.random() * 10;
+      return randomValue.toString();
+    }
   } else {
     if (numberOrOperator < 64) {
       return newTree(player) + " + " + newTree(player);
@@ -33,33 +39,61 @@ function newTree(player: { [index: string]: IPlayer }): string {
   }
 }
 
-function calculateWins(roster: any, fitness: any): number {
-  let cumulativeValue = 0;
-  roster.map((player: IPlayer) => {
-    const playerEquation = fitness(player);
-    const playerValue = eval(playerEquation);
-    cumulativeValue = cumulativeValue + playerValue;
+function calculateWins(
+  roster: IPlayer[],
+  population: Function[],
+  teamWins: number
+): number {
+  let allValues: IResult[] = [];
+  population.map((child: Function) => {
+    let childValue = 0;
+    roster.map((player: IPlayer) => {
+      const playerEquation = child(player);
+      const playerValue = eval(playerEquation);
+      childValue = childValue + playerValue;
+    });
+    if (childValue < teamWins && childValue <= 0) {
+      childValue = (childValue + teamWins) * -1;
+    } else if (childValue < teamWins && childValue > 0) {
+      childValue = teamWins - childValue;
+    } else if (childValue >= teamWins) {
+      childValue = childValue - teamWins;
+    } else {
+      childValue = 99999999999999999999999999999999999;
+    }
+    allValues.push({
+      value: childValue,
+      child: child.toString()
+    });
   });
-  return cumulativeValue;
+  allValues.sort(function(a, b) {
+    return a.value - b.value;
+  });
+  console.log(allValues);
+  return allValues[0].value;
 }
 
-function createPopulation(): IPopulation {
-  return function(player: { [index: string]: IPlayer }): string {
-    let num: number = 0;
-    let i: number;
-    let population = [];
-    for (i = num; i < populationSize; i++) {
-      population.push(newTree(player));
-    }
-    return population;
-  };
+function createPopulation(): Function[] {
+  let num: number = 0;
+  let i: number;
+  let population: Function[] = [];
+  for (i = num; i < populationSize; i++) {
+    const fitnessFunction = function(player: {
+      [index: string]: IPlayer;
+    }): string {
+      return newTree(player);
+    };
+    population.push(fitnessFunction);
+  }
+  return population;
 }
 
 export function getAlgorithmResults(req: Request, res: Response) {
   const winPercent = winsArray[2018]["MIN"];
   const roster = allStats[2018]["MIN"];
-  const fitnessFunction = createPopulation();
-  const wins = calculateWins(roster, fitnessFunction);
+  const population = createPopulation();
+  const teamWins = winsArray[2018]["MIN"];
+  const wins = calculateWins(roster, population, teamWins);
   res.send({ express: wins });
 }
 
